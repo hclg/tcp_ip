2925060827e10822f7893de21e023e2b1ee0a0f3
hclg <2249121951@qq.com>
2020-02-01 23:08:11 +0800

    套接字读取缓冲大小

--- 套接字的可选项/README.md
+++ 套接字的可选项/README.md
@@ -46,6 +46,22 @@ int setsockopt(int sock, int level, int optname, const void *optval, socklen_t o
 
 调用getsockopt函数的事例如下：
 
-* 
+* [sokc_type](https://github.com/hclg/tcp_ip/blob/master/%E5%A5%97%E6%8E%A5%E5%AD%97%E7%9A%84%E5%8F%AF%E9%80%89%E9%A1%B9/sock_type.c)
+* 注意事项: 运行有时候会发生错误，多运行几次即可。
+* 套接字类型只能在创建决定，后期不能更改。
 
 ![image-20200201215833438](/home/h1/.config/Typora/typora-user-images/image-20200201215833438.png)
+
+##### 1.3 I/O缓冲大小
+
+​	生成套接字的时候将同时生成I/O缓冲。
+
+​	SO_RCVBUF是输入缓冲大小可选项，SO_SNDBUF是输出缓冲大小可选项
+
+读取与更改事例
+
+读取
+
+* 
+
+![image-20200201230647194](/home/h1/.config/Typora/typora-user-images/image-20200201230647194.png)
---
+++ 套接字的可选项/get_buf.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+void error(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+int main() {
+    int sock;
+    int snd_buf, rcv_buf, state;
+    socklen_t len;
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    len = sizeof(snd_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+    if (state)
+        error("getsockopt() error");
+    len = sizeof(rcv_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&rcv_buf, &len);
+    if (state)
+        error("getsockopt() error");
+    printf("INPUT buffer: %d\n", rcv_buf);
+    printf("OUTput buffer: %d\n", snd_buf);
+    close(sock);
+    return 0;
+}
